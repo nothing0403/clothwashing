59d6377be087ffb3355625af2beed1fba520641f
User <User@DESKTOP-IK27FDM>
2025-06-18 02:24:28 +0800

    0618

--- src/main/java/com/example/demo/controller/LoginRestController.java
+++ src/main/java/com/example/demo/controller/LoginRestController.java
@@ -40,6 +40,7 @@ import com.example.demo.model.entity.User;
 import com.example.demo.response.ApiResponse;
 import com.example.demo.service.ClothService;
 import com.example.demo.service.ContentService;
+import com.example.demo.service.EmailService;
 import com.example.demo.service.ItemService;
 import com.example.demo.service.ReceiverService;
 import com.example.demo.service.SenderService;
@@ -62,58 +63,42 @@ public class LoginRestController {
 	@Autowired
 	private ContentService contentService;
 	
+	@Autowired
+	private EmailService emailService;
+	
 	private String authcode;
-	
-	private String generateAuthCode() {
-		String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		StringBuffer authcode = new StringBuffer();
-		Random random = new Random();
-		for(int i=0;i<4;i++) {
-			int index = random.nextInt(chars.length()); // 隨機取位置
-			authcode.append(chars.charAt(index)); // 取得該位置的資料
-		}
-		return authcode.toString();
-	}
-	
-	private BufferedImage getAuthCodeImage(String authcode) {
-		// 建立圖像區域(80x30 TGB)
-		BufferedImage img = new BufferedImage(80, 30, BufferedImage.TYPE_INT_RGB);
-		// 建立畫布
-		Graphics g = img.getGraphics();
-		// 設定顏色
-		g.setColor(Color.YELLOW);
-		// 塗滿背景
-		g.fillRect(0, 0, 80, 30); // 全區域
-		// 設定顏色
-		g.setColor(Color.BLACK);
-		// 設定字型
-		g.setFont(new Font("Segoe UI Emoji", Font.BOLD, 22)); // 字體, 風格, 大小
-		// 繪文字
-		g.drawString(authcode, 18, 22); // (18, 22) 表示繪文字左上角的起點
-		// 加上干擾線
-		g.setColor(Color.RED);
-		Random random = new Random();
-		for(int i=0;i<15;i++) {
-			// 座標點
-			int x1 = random.nextInt(80); // 0~79
-			int y1 = random.nextInt(30); // 0~29
-			int x2 = random.nextInt(80); // 0~79
-			int y2 = random.nextInt(30); // 0~29
-			// 繪直線
-			g.drawLine(x1, y1, x2, y2);
-		}
-		return img;
-	}
 	
 	@GetMapping("/home")
 	public ResponseEntity<ApiResponse<List<ClothDto>>> home(HttpSession session){
 		
-		List<ClothDto> clothDtos = clothService.getCloth();
+		
+		List<ClothDto> clothDtos = session.getAttribute("clothDtos") == null? clothService.getCloth(): (List<ClothDto>)session.getAttribute("clothDtos");
+	
 		
 		return ResponseEntity.ok(ApiResponse.success(null, clothDtos));
 	}
 	
-	@PostMapping("/deliver")
+	@PostMapping("/update")
+	public ResponseEntity<ApiResponse<Void>> update(@RequestBody List<ClothDto> clothDtos, HttpSession session){
+		
+		session.setAttribute("clothDtos", clothDtos);
+		
+		List<ClothDto> filterClothDtos = clothDtos.stream().filter(clothDto -> clothDto.getClothQuantity()!=0).collect(Collectors.toList());
+		
+		session.setAttribute("filterClothDtos", filterClothDtos);
+		
+		return ResponseEntity.ok(ApiResponse.success(null, null));
+	}
+	
+	@GetMapping("/deliver")
+	public ResponseEntity<ApiResponse<DeliverDto>> deliver(HttpSession session){
+		
+		DeliverDto deliverDto = session.getAttribute("deliverDto") == null? new DeliverDto(): (DeliverDto)session.getAttribute("deliverDto");
+		
+		return ResponseEntity.ok(ApiResponse.success(null, deliverDto));
+	}
+	
+	@PostMapping("/update/deliver")
 	public ResponseEntity<ApiResponse<Void>> deliver(@RequestBody DeliverDto deliverDto, HttpSession session){
 		
 		session.setAttribute("deliverDto", deliverDto);
@@ -124,7 +109,7 @@ public class LoginRestController {
 	@GetMapping("/cart")
 	public ResponseEntity<ApiResponse<CartDto>> cart(HttpSession session){
 		
-		List<ClothDto> clothDtos = (List<ClothDto>)session.getAttribute("clothDtos");
+		List<ClothDto> clothDtos = (List<ClothDto>)session.getAttribute("filterClothDtos");
 		
 		DeliverDto deliverDto = (DeliverDto)session.getAttribute("deliverDto");
 		
@@ -151,13 +136,12 @@ public class LoginRestController {
 		return ResponseEntity.ok(ApiResponse.success(null, cartDto));
 	}
 	
-	
-	@PostMapping("/update")
-	public ResponseEntity<ApiResponse<Void>> update(@RequestBody List<ClothDto> clothDtos, HttpSession session){
+	@GetMapping("/result")
+	public ResponseEntity<ApiResponse<Void>> result(HttpSession session){
 		
-		List<ClothDto> filterClothDtos = clothDtos.stream().filter(clothDto -> clothDto.getClothQuantity()!=0).collect(Collectors.toList());
+		UserDto userDto = (UserDto)session.getAttribute("userDto");
 		
-		session.setAttribute("clothDtos", filterClothDtos);
+		emailService.sendEmail(userDto.getUserAccount());
 		
 		return ResponseEntity.ok(ApiResponse.success(null, null));
 	}
@@ -205,4 +189,45 @@ public class LoginRestController {
 		
 		return ResponseEntity.ok(ApiResponse.success("註冊成功", null));
 	} 
+	
+	private String generateAuthCode() {
+		String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		StringBuffer authcode = new StringBuffer();
+		Random random = new Random();
+		for(int i=0;i<4;i++) {
+			int index = random.nextInt(chars.length()); // 隨機取位置
+			authcode.append(chars.charAt(index)); // 取得該位置的資料
+		}
+		return authcode.toString();
+	}
+	
+	private BufferedImage getAuthCodeImage(String authcode) {
+		// 建立圖像區域(80x30 TGB)
+		BufferedImage img = new BufferedImage(80, 30, BufferedImage.TYPE_INT_RGB);
+		// 建立畫布
+		Graphics g = img.getGraphics();
+		// 設定顏色
+		g.setColor(Color.YELLOW);
+		// 塗滿背景
+		g.fillRect(0, 0, 80, 30); // 全區域
+		// 設定顏色
+		g.setColor(Color.BLACK);
+		// 設定字型
+		g.setFont(new Font("Segoe UI Emoji", Font.BOLD, 22)); // 字體, 風格, 大小
+		// 繪文字
+		g.drawString(authcode, 18, 22); // (18, 22) 表示繪文字左上角的起點
+		// 加上干擾線
+		g.setColor(Color.RED);
+		Random random = new Random();
+		for(int i=0;i<15;i++) {
+			// 座標點
+			int x1 = random.nextInt(80); // 0~79
+			int y1 = random.nextInt(30); // 0~29
+			int x2 = random.nextInt(80); // 0~79
+			int y2 = random.nextInt(30); // 0~29
+			// 繪直線
+			g.drawLine(x1, y1, x2, y2);
+		}
+		return img;
+	}
 }
